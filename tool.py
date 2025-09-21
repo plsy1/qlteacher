741b1860908e86426fb135782601a8995966acb3
y1 <39407459+plsy1@users.noreply.github.com>
2025-09-21 17:22:57 +0800

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,15 +1,18 @@
 import requests
 import base64
 
+
 def get_user_input():
     username = input("账号: ")
     password = input("密码: ")
     return username, password
 
+
 def encode_password(password):
-    password_bytes = password.encode('utf-8')
-    base64_encoded = base64.b64encode(password_bytes).decode('utf-8')
-    return '3' + base64_encoded
+    password_bytes = password.encode("utf-8")
+    base64_encoded = base64.b64encode(password_bytes).decode("utf-8")
+    return "3" + base64_encoded
+
 
 def get_token(username, pwd):
     headers = {
@@ -17,12 +20,7 @@ def get_token(username, pwd):
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
     }
 
-    data = {
-        "username": username,
-        "password": pwd,
-        "grant_type": "password",
-        "pws": "b"
-    }
+    data = {"username": username, "password": pwd, "grant_type": "password", "pws": "b"}
 
     url = "https://id.qlteacher.com/api/auth/token"
     response = requests.post(url, headers=headers, data=data)
@@ -30,10 +28,11 @@ def get_token(username, pwd):
     if response.status_code != 200:
         print("登录失败，请检查密码")
         exit()
-        
+
     data = response.json()
     token = data["access_token"]
     return token
+
 
 def get_course_ids(token):
     headers = {
@@ -41,40 +40,47 @@ def get_course_ids(token):
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
     }
 
-    response = requests.get("https://yanxiu.qlteacher.com/api/project/xx2023/user/me", headers=headers)
+    response = requests.get(
+        "https://yxjc.qlteacher.com/api//lesson/lessons/my?projectId=xx2025",
+        headers=headers,
+    )
 
     if response.status_code == 200:
         data = response.json()
-        courses = data["lessonList"]
-        course_ids = [course["courseId"] for course in courses]
-        return course_ids
+        data = data["data"]
+        lesson_ids = [
+            lesson["id"] for topic in data for lesson in topic.get("lessons", [])
+        ]
+        return lesson_ids
     else:
         print("课程获取失败")
         exit()
 
-def get_video_ids(course_ids, token):
-    base_url = 'https://yanxiu.qlteacher.com/api/project/xx2023/learning/'
-    video_ids = []
+
+def get_video_ids(first_id, token):
+    url = f"https://yxjc.qlteacher.com/api//lesson/learning/url?projectId=xx2025&lessonId={first_id}"
 
     headers = {
         "Authorization": f"Bearer {token}",
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
     }
 
-    for course_id in course_ids:
-        url = base_url + course_id
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            video_url = data.get("data")
-            if video_url:
-                video_id = video_url.split("/")[-1]
-                video_ids.append(video_id)
-        else:
-            print("一级课程ID获取失败")
-            exit()
+    res = requests.get(url, headers=headers)
+    link = res.json()["data"]
+    id_str = link.split("/")[-1]
+    tmp = f"https://player.qlteacher.com/api/learning/{id_str}/course"
 
-    return video_ids
+    res = requests.get(tmp, headers=headers)
+    data = res.json()["data"]
+
+    activity_ids = [
+        act["id"]
+        for sec in data.get("sections", [])
+        for act in sec.get("activitys", [])
+    ]
+
+    return id_str, activity_ids
+
 
 def update_video_duration(video, activity, act_token, token):
     headers = {
@@ -83,66 +89,55 @@ def update_video_duration(video, activity, act_token, token):
     }
 
     url = f"https://player.qlteacher.com/api/learning/{video}/duration/video/{activity}/{act_token}/-1?_ignore_error=true"
-    data = "{}"  
+    data = "{}"
     response = requests.put(url, headers=headers, data=data)
 
     if response.status_code == 200:
         print("刷课成功")
+
 
 def main():
     username, password = get_user_input()
     pwd = encode_password(password)
     token = get_token(username, pwd)
     course_ids = get_course_ids(token)
-    video_ids = get_video_ids(course_ids, token)
-
-
 
     headers = {
         "Authorization": f"Bearer {token}",
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
     }
 
+    for courseID in course_ids:
+        realCourseID, video_ids = get_video_ids(courseID, token)
+        for videoID in video_ids:
+            print("当前刷课ID：", videoID)
+            url = f"https://player.qlteacher.com/api/learning/{realCourseID}/activity/{videoID}"
+            response = requests.get(url, headers=headers)
+            if response.status_code != 200:
+                print("获取actToken失败")
+                exit()
 
-    for video in video_ids:
-        print("当前刷课的一级课程ID：", video)
-        url = f"https://player.qlteacher.com/api/learning/{video}/course"
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
             data = response.json()
-            activitys = data["data"]["sections"][0]["activitys"]
-            activity_ids = [activity["id"] for activity in activitys]
-            print("成功取得二级课程ID，开始刷课")
-            
-            for activity in activity_ids:
-                print("当前刷课的二级课程ID：", activity)
-                url = f"https://player.qlteacher.com/api/learning/{video}/activity/{activity}"
-                response = requests.get(url, headers=headers)
-                if response.status_code != 200:
-                    print("获取actToken失败")
-                    exit()
 
-                data = response.json()
-                act_token = str(data["data"]["actToken"])
-                length = data["data"]["activity"]["standard"]
-                times = length / 60
-                offset = 0
+            act_token = str(data["data"]["actToken"])
 
-                while times > 1:
-                    offset += 60
-                    url = f"https://player.qlteacher.com/api/learning/{video}/duration/video/{activity}/{act_token}/{offset}?_ignore_error=true"
-                    data = "{}"  # You can modify this if needed
-                    response = requests.put(url, headers=headers, data=data)
-                    if response.status_code == 200:
-                        print("时长已更新,当前值（分钟）：", offset / 60)
-                    times = times - 1
+            length = data["data"]["activity"]["standard"]
+            times = length / 60
+            offset = 0
 
-                update_video_duration(video, activity, act_token, token)
-        else:
-            print("二级课程ID获取失败")
-            exit()
+            while times > 1:
+                offset += 60
+                url = f"https://player.qlteacher.com/api/learning/{realCourseID}/duration/video/{videoID}/{act_token}/{offset}?_ignore_error=true"
+                data = "{}"  # You can modify this if needed
+                response = requests.put(url, headers=headers, data=data)
+                if response.status_code == 200:
+                    print("时长已更新,当前值：", offset / 60)
+                times = times - 1
+
+            update_video_duration(realCourseID, videoID, act_token, token)
 
     print("当前账号刷课全部完成！")
+
 
 if __name__ == "__main__":
     main()
